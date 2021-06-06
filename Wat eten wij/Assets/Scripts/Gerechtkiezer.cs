3cc8ff094c1c290aea71e86d099dfaf7536eafb5
Eran van der Horst <329741@student.mboutrecht.nl>
2021-06-06 20:06:30 +0200

    Done

--- Wat eten wij/Assets/Scripts/Gerechtkiezer.cs
+++ Wat eten wij/Assets/Scripts/Gerechtkiezer.cs
@@ -8,6 +8,8 @@ public class Gerechtkiezer : MonoBehaviour
     [SerializeField] private List<string> Gerechten;
     
     public Button Starter;
+    public Button Saved;
+    public Button SaveRecipe;
     
     public GameObject Zondag;
     public GameObject Maandag;
@@ -24,10 +26,20 @@ public class Gerechtkiezer : MonoBehaviour
     public int donderdagGerecht;
     public int vrijdagGerecht;
     
+    public int savedZondagGerecht;
+    public int savedMaandagGerecht;
+    public int savedDinsdagGerecht;
+    public int savedWoensdagGerecht;
+    public int savedDonderdagGerecht;
+    public int savedVrijdagGerecht;
+    
+    
 
     void Start()
     {
         Starter.onClick.AddListener(NewRecipes);
+        Saved.onClick.AddListener(LoadRecipes);
+        SaveRecipe.onClick.AddListener(Save);
     }
 
     void NewRecipes()
@@ -46,6 +58,47 @@ public class Gerechtkiezer : MonoBehaviour
         Woensdag.GetComponent<Text>().text = Gerechten[woensdagGerecht];
         Donderdag.GetComponent<Text>().text = Gerechten[donderdagGerecht];
         Vrijdag.GetComponent<Text>().text = Gerechten[vrijdagGerecht];
+        Zaterdag.GetComponent<Text>().text = "Patat";
+    }
+
+    void Save()
+    {
+        savedZondagGerecht = zondagGerecht;
+        savedMaandagGerecht = maandagGerecht;
+        savedDinsdagGerecht = dinsdagGerecht;
+        savedWoensdagGerecht = woensdagGerecht;
+        savedDonderdagGerecht = donderdagGerecht;
+        savedVrijdagGerecht = vrijdagGerecht;
+        
+        PlayerPrefs.SetInt("Zondag", savedZondagGerecht);
+        PlayerPrefs.SetFloat("Maandag", savedMaandagGerecht);
+        PlayerPrefs.SetFloat("Dinsdag", savedDinsdagGerecht);
+        PlayerPrefs.SetFloat("Woensdag", savedWoensdagGerecht);
+        PlayerPrefs.SetInt("Donderdag", savedDonderdagGerecht);
+        PlayerPrefs.SetInt("Vrijdag", savedVrijdagGerecht);
+        Debug.Log(zondagGerecht);
+        Debug.Log(savedZondagGerecht);
+    }
+
+    void LoadRecipes()
+    {
+        
+        PlayerPrefs.GetInt("Zondag", savedZondagGerecht);
+        PlayerPrefs.GetFloat("Maandag", savedMaandagGerecht);
+        PlayerPrefs.GetFloat("Dinsdag", savedDinsdagGerecht);
+        PlayerPrefs.GetFloat("Woensdag", savedWoensdagGerecht);
+        PlayerPrefs.GetInt("Donderdag", savedDonderdagGerecht);
+        PlayerPrefs.GetInt("Vrijdag", savedVrijdagGerecht);
+        Debug.Log(zondagGerecht);
+        Debug.Log(savedZondagGerecht);
+
+
+        Zondag.GetComponent<Text>().text = Gerechten[savedZondagGerecht];
+        Maandag.GetComponent<Text>().text = Gerechten[savedMaandagGerecht];
+        Dinsdag.GetComponent<Text>().text = Gerechten[savedDinsdagGerecht];
+        Woensdag.GetComponent<Text>().text = Gerechten[savedWoensdagGerecht];
+        Donderdag.GetComponent<Text>().text = Gerechten[savedDonderdagGerecht];
+        Vrijdag.GetComponent<Text>().text = Gerechten[savedVrijdagGerecht];
         Zaterdag.GetComponent<Text>().text = "Patat";
     }
 }
